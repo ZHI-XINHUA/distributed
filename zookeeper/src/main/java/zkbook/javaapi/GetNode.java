d4397b1ee1001b3187affc9644f79d38c9560436
ZHI-XINHUA <1960881192@qq.com>
2018-09-06 21:19:13 +0800

    查询节点

--- zookeeper/src/main/java/zkbook/javaapi/GetNode.java
+++ zookeeper/src/main/java/zkbook/javaapi/GetNode.java
@@ -1,9 +1,8 @@
 package zkbook.javaapi;
 
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.List;
@@ -12,42 +11,36 @@ import java.util.concurrent.TimeUnit;
 
 public class GetNode {
     private static ZooKeeper zooKeeper = null;
-    //服务器连接地址
-    private static  String host = "192.168.1.106:2181,192.168.1.108:2181,192.168.1.109:2181";
-    //超时时间
-    private static int sessionTimeout=5000;
 
-    public  static CountDownLatch countDownLatch = new CountDownLatch(1);
-
-
-    static {
-        try {
-            //连接服务器
-            zooKeeper = new ZooKeeper(host,sessionTimeout,new MyGetWatch());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) throws InterruptedException {
-        countDownLatch.await();
+        //getNode();
 
-        getNode1();
+        getChildNode();
     }
 
-    private static void getNode1(){
+    /**
+     * 获取节点
+     */
+    private static void getNode(){
+       zooKeeper = ConnectionZk.getZkConnection();
+
+       String path = "/mydata";
         try {
+            byte[] bytes1 = zooKeeper.getData(path,true,null);
+            System.out.println("节点【"+path+"】的值为："+new String(bytes1));
 
-            //获取子节点列表
-            List<String> childNodes= zooKeeper.getChildren("/",false);
-            System.out.println("根目录子节点列表："+childNodes);
+            System.out.println("================================");
+            byte[] bytes2 = zooKeeper.getData(path,new GetNodeWatcher(),null);
+            System.out.println("节点【"+path+"】的值为："+new String(bytes2));
 
-            List<String> childNodes1=zooKeeper.getChildren("/",new MyGetWatch());
-            System.out.println("根目录子节点列表："+childNodes1);
+            System.out.println("================================");
+            zooKeeper.getData(path,true,new GetDataCallback(),"get Node");
 
+            System.out.println("================================");
+            zooKeeper.getData(path,new GetNodeWatcher(),new GetDataCallback(),"get Node xxx");
 
-           TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+            TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
         } catch (KeeperException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -55,21 +48,81 @@ public class GetNode {
         }
     }
 
-}
+    /**
+     * 获取子节点
+     */
+    private static void getChildNode(){
+        zooKeeper = ConnectionZk.getZkConnection();
 
-class  MyGetWatch implements Watcher {
-    @Override
-    public void process(WatchedEvent watchedEvent) {
-        System.out.println(watchedEvent.getState());
-        if(Event.KeeperState.SyncConnected==watchedEvent.getState()){
-            System.out.println("ZooKeeper 链接成功！");
+        String path = "/mydata";
+        try {
+            List<String>  childNodes = zooKeeper.getChildren(path,true);
+            System.out.println(path+"子节点有："+childNodes);
+            System.out.println("=========================");
 
+            List<String>  childNodes1 = zooKeeper.getChildren(path,new GetNodeWatcher());
+            System.out.println(path+"子节点有："+childNodes1);
+            System.out.println("=========================");
 
-            if(watchedEvent.getType()==Event.EventType.None && watchedEvent.getPath()==null){
-                GetNode.countDownLatch.countDown();
-            }else if(watchedEvent.getType()==Event.EventType.NodeChildrenChanged){
-                System.out.println(watchedEvent.getPath());
-            }
+            zooKeeper.getChildren(path,new GetNodeWatcher(),new MyChildrenCallback()," call ChildrenCallback");
+
+            System.out.println("=========================");
+            zooKeeper.getChildren(path,new GetNodeWatcher(),new MyChildren2Callback()," call MyChildren2Callback");
+
+            TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
+
+/**
+ * 事件监听
+ */
+class GetNodeWatcher implements Watcher{
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        System.out.println(watchedEvent.getType());
+        String path = watchedEvent.getPath();
+        if(watchedEvent.getType()== Event.EventType.NodeCreated){
+            System.out.println("创建节点【"+path+"】");
+        }else if(watchedEvent.getType()== Event.EventType.NodeDataChanged){
+            System.out.println("修改节点【"+path+"】");
+        }else if(watchedEvent.getType()== Event.EventType.NodeDeleted){
+            System.out.println("删除节点【"+path+"】");
+        }else if(watchedEvent.getType()==Event.EventType.NodeChildrenChanged){//创建/删除子节点 触发，修改子节点不触发
+            System.out.println("【"+path+"】创建/删除子节点");
+        }
+    }
+}
+
+/**
+ * 回调
+ */
+class GetDataCallback implements AsyncCallback.DataCallback{
+    @Override
+    public void processResult(int rc, String path, Object ctx, byte[] bytes, Stat stat) {
+        System.out.println("StringCallback:[rc="+rc+",path="+path+",ctx="+ctx+",stat="+stat+",bytes="+new String(bytes)+"]");
+    }
+}
+
+class MyChildrenCallback implements AsyncCallback.ChildrenCallback{
+
+    @Override
+    public void processResult(int rc, String path, Object ctx, List<String> list) {
+        System.out.println("MyChildrenCallback:[rc="+rc+",path="+path+",ctx="+ctx+",list="+list);
+    }
+}
+
+class MyChildren2Callback implements AsyncCallback.Children2Callback{
+
+    @Override
+    public void processResult(int rc, String path, Object ctx, List<String> list, Stat stat) {
+        System.out.println("MyChildren2Callback:[rc="+rc+",path="+path+",ctx="+ctx+",list="+list+",stat="+stat);
+    }
+}
+
+
