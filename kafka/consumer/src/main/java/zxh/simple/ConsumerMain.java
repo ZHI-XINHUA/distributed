4ab79dea102f56bcfe2a082d0816a13f8aa32bda
ZHI-XINHUA <1960881192@qq.com>
2018-12-10 18:15:13 +0800

    kafka module

---
+++ kafka/consumer/src/main/java/zxh/simple/ConsumerMain.java
@@ -0,0 +1,86 @@
+package zxh.simple;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by xh.zhi on 2018-12-10.
+ */
+public class ConsumerMain {
+    private static final String SERVERS = "192.168.3.31:9092,192.168.3.3.46:9092,192.168.3.118:9092";
+    private static void initConsumer() throws Exception{
+        Properties consumerConfig = new Properties();
+        //该属性指定 broker 的地址清单;集群中建议两个以上，避免一个宕机则不可用
+        consumerConfig.put("bootstrap.servers",SERVERS);
+        //指定消费组
+        consumerConfig.put("group.id", "group1");
+        //指定key序列化类
+        consumerConfig.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        //指定value序列化类
+        consumerConfig.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        //设定自动提交
+        consumerConfig.put("enable.auto.commit", "false");
+
+        //other config
+        //指定了消费者从服务器获取记录的最小字节数。 broker 在收到消费者的数据请求时，如果可用的数据量小于指定的大小，那么它会等到有足够的可用数据时才把它返回给消费者
+        consumerConfig.put("fetch.min.bytes","1");
+        //指定 broker 的等待时间,默认是 500ms
+        consumerConfig.put("fetch.max.wa it.ms",1000);
+        //指定了服务器从每个分区里返回给消费者的最大字节数。它的默认值是 lMB;也就是说， consumer.poll方住从每个分区里返回的记录最多不超过 指定的字节
+        consumerConfig.put("max.partition.fetch.bytes",3*1024*1024);
+        //指定了消费者在被认为死亡之前可以与服务器断开连接的时间，默认是 3s
+        consumerConfig.put("session.timeout.ms",5000);
+        //指定了消费者在读取一个没有偏移量的分区或者偏移量无效的情况下（因消费者长时间失效，包含偏移量的记录已经过时井被删除）该作何处理
+        //consumerConfig.put("auto.offset.reset","latest");
+        //可以是任意字符串 ， broker 用它来标识从客户端发送过来的消息，通常被用在日志、度量指标和配额里。
+        consumerConfig.put("client.id","client-consumer");
+        //分配策略:给定的消费者和主题，决定哪些分区应该被分配给哪个消费者
+        consumerConfig.put("partition.assignment.strategy","org.apache.kafka.clients.consumer.RangeAssignor");
+        //属性用于控制单次调用 call （） 方住能够返回的记录数量，可以帮你控制在轮询里需要处理的数据量。
+        consumerConfig.put("max.poll.records",50);
+        //设置socket读写数据时TCP缓冲区的大小
+        consumerConfig.put("receive.buffer.bytes",1*1024*1024);
+        consumerConfig.put("send.buffer.bytes",1*1024*1024);
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(consumerConfig);
+
+        //订阅主题列表；支持表达式：如 consumer.subscribe("test*")
+        consumer.subscribe(Arrays.asList("my_topic_test","zxh"));
+
+        int minBatchSize = 200;
+
+        try{
+            //轮询读取消息
+            while (true){
+                /**消费者必须持续对 Kafka 进行轮询，否则会被认为己经死亡 ， 它的分区会被移交给群组里的其他消费者。传给
+                poll （） 方法的参数是一个超时时间，用于控制 poll （） 方陆的阻塞时间（在消费者的缓
+                冲区里没有可用数据时会发生阻塞）。如果该参数被设为 0, poll （） 会立即返回 ，否则
+                它会在指定的毫秒数内一直等待 broker 返回数据。**/
+                ConsumerRecords<String,String> records =  consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records){
+                    System.out.printf("offset = %d, key = %s, value = %s\n",
+                            record.offset(), record.key(), record.value());
+                }
+
+            }
+        }finally {
+            consumer.close();
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            initConsumer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ kafka/provider/src/main/java/zxh/simple/ProviderMain.java
@@ -0,0 +1,105 @@
+package zxh.simple;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * 生产端demo
+ */
+public class ProviderMain {
+    private static final String SERVERS = "192.168.3.31:9092,192.168.3.3.46:9092,192.168.3.118:9092";
+
+    private static  void initProducer() throws InterruptedException {
+        Properties provoderConfig = new Properties();
+        //该属性指定 broker 的地址清单;集群中建议两个以上，避免一个宕机则不可用
+        provoderConfig.put("bootstrap.servers",SERVERS);
+        //key序列化类
+        provoderConfig.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        //value序列化类
+        provoderConfig.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        //消息发送可靠性配置：生产者发送消息到broker，确认方式；（0：producer不会等待broker（leader）发送ack；acks=1：当Leader接收到消息之后发送ack；当所有的follower都同步消息成功后发送ack）
+        provoderConfig.put("acks","0");
+        //设置生产者内存缓冲区的大小，生产者用它缓冲要发送到服务器的消息
+        provoderConfig.put("buffer.memory",32*1024*1024);
+        //消息压缩配置，默认不会压缩。（none, gzip, snappy, lz4, zstd）
+        provoderConfig.put("compression.type","gzip");
+        //发送失败时重发次数
+        provoderConfig.put("retries",5);
+        //指定了一个批次可以使用的内存大小
+        provoderConfig.put("batch.size",16384);
+        //指定了生产者在发送批次之前等待更多消息加入批次的时间
+        //provoderConfig.put("linger.ms",50);
+        //可以是任意的字符串，服务器会用它来识别消息的来橱，还可以用在日志和配额指标里。
+        provoderConfig.put("client.id","zxh-proder");
+        //参数指定了生产者在收到服务器晌应之前可以发送多少个消息；它的值越高，就会占用越多的内存，不过也会提升吞吐量。 把它设为 1 可以保证消息是按照发送的顺序写入服务器的，即使发生了重试。
+        provoderConfig.put("max.in.flight.requests.per.connection",1);
+        //控制生产者发送的请求大小;它可以指能发送的单个消息的最大值，也可以指单个请求里所有消息总的大小
+        provoderConfig.put("max.request.size",1*1024*1024);
+
+        //创建生成者
+        Producer<String,String> producer = new KafkaProducer<String, String>(provoderConfig);
+
+        String topic = "my_topic_test";
+        for(int i=0;i<100;i++){
+            ProducerRecord producerRecord =  new ProducerRecord<String, String>(topic,"key_"+i,"value_"+i);
+
+            //返送消息记录到指定topic中
+            //消息先是被放进缓冲区，然后使用单独的线程发送到服务器端。
+           // Future<RecordMetadata> future =  producer.send(producerRecord);
+            //同步发送
+//            try {
+//                //get()等待 Kafka 响应。如果服务器返回错误， get 方怯会抛出异常。如果没有发生错误，我们会得到一个RecordMetadata对象，可以用它获取消息的偏移量。
+//                future.get();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            }
+
+            //异步发送
+            producer.send(producerRecord,new ProviderCallback());
+
+            Thread.sleep(100);
+        }
+
+        //关闭
+        producer.close();
+
+    }
+    public static void main(String[] args) {
+        try {
+            initProducer();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 异步发送的回调
+     */
+    private static class ProviderCallback implements Callback{
+
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            //发送异常，坐相应的处理：如重发等
+            if(e!=null){
+                e.printStackTrace();
+
+                return;
+            }
+
+            //主题
+            String topic = recordMetadata.topic();
+            //分区
+            int partition = recordMetadata.partition();
+            //偏移量
+            long offset = recordMetadata.offset();
+
+            System.out.println(" top="+topic +"\n partition="+partition +"\n" +
+                    " hasOffset="+recordMetadata.hasOffset()+" \n offset="+offset+"\n toString="+ recordMetadata.toString());
+            System.out.println();
+        }
+    }
+}
